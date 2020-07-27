7d77898ab094904c8bd2e979cb1e59d1be158375
aiden <1098003693@qq.com>
2020-07-27 15:13:01 +0800

    'api-test'

--- routes/api.php
+++ routes/api.php
@@ -16,5 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware'=>['api']],function(){
     Route::any('ocpctrans','AidenController@ocpcUp');
+    Route::any('ocpctranstest','AidenController@ocpcUpTest');
 
 });
